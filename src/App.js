52b70c7a035da19145f4eef22ef4739e18a474a9
CidRobles <cidrobles.cesar@gmail.com>
2020-09-15 23:12:23 -0500

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+// Pages
+import Home from './pages/home';
+import Skills from './pages/skills';
+import Contact from './pages/contact';
 
 function App() {
   return (
-    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
-    <div className="ml-6 pt-1">
-      <h1 className="text-2xl text-blue-700 leading-tight">
-        Tailwind and Create React App
-      </h1>
-      <p className="text-base text-gray-700 leading-normal">
-        Building apps together
-      </p>
+    <div className="h-screen">
+      <Router>
+        <header>
+          <div className="container mx-auto">
+            <ul className="grid grid-cols-3 gap-5">
+              <li className="p-5 text-center antialiased text-gray-500 text-xl transition-shadow duration-500 transform hover:shadow-xl">
+                <Link to="/" className="block w-full h-full">Home</Link>
+              </li>
+              <li className="p-5 text-center antialiased text-gray-500 text-xl transition-shadow duration-500 transform hover:shadow-xl">
+                <Link to="/skills" className="block w-full h-full">Skills</Link>
+              </li>
+              <li className="p-5 text-center antialiased text-gray-500 text-xl transition-shadow duration-500 transform hover:shadow-xl">
+                <Link to="/contact" className="block w-full h-full">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </header>
+        <main className="container mx-auto p-5">
+          <Switch>
+            <Route path="/skills">
+              <Skills />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
     </div>
-  </div>
   );
 }
 
---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// Form component
+import ContactForm from '../components/contactForm';
+
+function Contact() {
+  return (    
+    <ContactForm></ContactForm>
+  );
+}
+
+export default Contact;
